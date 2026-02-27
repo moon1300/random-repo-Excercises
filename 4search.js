cbf7484c2c574c20765493892669231f154c0729
moon1300 <n11180021@qut.edu.au>
2026-02-27 20:47:04 +1000

    serch file created

---
+++ 4search.js
@@ -0,0 +1,13 @@
+// Excersise 4: Search books
+
+// Write a function searchBooks which returns ALL the books written by a 
+// specific author and return the list of the book titles as a string, 
+// separated with comma if there are more than one titles. 
+// If these is no author in the library then return 'NOT FOUND'. 
+// Example dataset: library = [ { author: 'Bill Gates', title: 'The Road Ahead', 
+// libraryID: 1254} ]; Example authorName='Bill Gates'.
+
+function searchBooks(library, authorName) {
+}
+
+// in repl:
