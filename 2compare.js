3410b890ee15f07195f2210acc5af3ce8b5fe109
moon1300 <n11180021@qut.edu.au>
2026-02-27 17:24:26 +1000

    completed 2 excersises

---
+++ 2compare.js
@@ -0,0 +1,8 @@
+// Excersise 2: compare two values
+
+// Write a function compare which returns the boolean result of 
+// its two parameters by checking that they have equal value and equal type.
+
+function compare(x,y) {
+    return x === y;
+}
