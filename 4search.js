0c609331200396663452a4b856edae17a781ef10
moon1300 <n11180021@qut.edu.au>
2026-02-28 11:34:54 +1000

    created excersise 5 file

--- 4search.js
+++ 4search.js
@@ -1,13 +1,23 @@
 // Excersise 4: Search books
 
-// Write a function searchBooks which returns ALL the books written by a 
-// specific author and return the list of the book titles as a string, 
-// separated with comma if there are more than one titles. 
-// If these is no author in the library then return 'NOT FOUND'. 
-// Example dataset: library = [ { author: 'Bill Gates', title: 'The Road Ahead', 
-// libraryID: 1254} ]; Example authorName='Bill Gates'.
+/* Write a function searchBooks which returns ALL the books written by a 
+specific author and return the list of the book titles as a string, 
+separated with comma if there are more than one titles. 
+If these is no author in the library then return 'NOT FOUND'. 
+Example dataset: library = [ { author: 'Bill Gates', title: 'The Road Ahead', 
+libraryID: 1254} ]; Example authorName='Bill Gates'.
+*/
 
 function searchBooks(library, authorName) {
+    const booksByAuthor = library.filter(book => book.author === authorName);
+    if (booksByAuthor.length === 0) {
+        return 'NOT FOUND';
+    }
+    return booksByAuthor.map(book => book.title).join(', ');
 }
 
-// in repl:
+/* in repl:
+library = [ { author: 'Bill Gates', title: 'The Road Ahead', libraryID: 1254} ]
+searchBooks(library, 'Bill Gates') // 'The Road Ahead'
+searchBooks(library, 'Colleen Hoover') // 'NOT FOUND'
+*/
